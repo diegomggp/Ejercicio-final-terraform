d0afdfd1f8b7ae5d17cb2bb6688e110880f513fe
diego <diegomg456@gmail.com>
2024-06-26 08:37:40 +0200

    borro

--- modulos/dynamo/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "nombre_dynamoDB" {
-  description = "nombre de la base de datos en dynamoDb"
-  type        = string
-}
-
-variable "hash_key" {
-  description = "Columna principal de la dynamoDB"
-  type        = map(string)
-}
